2a48556c33a67a1a6f03cd5cdb5d99a49e9d6bef
petitviolet <violethero0820@gmail.com>
2017-01-09 23:05:45 +0900

    add ::

--- modules/performance/src/main/scala/net/petitviolet/sandbox/perf/SeqOrList.scala
+++ modules/performance/src/main/scala/net/petitviolet/sandbox/perf/SeqOrList.scala
@@ -2,27 +2,48 @@ package net.petitviolet.sandbox.perf
 
 import org.openjdk.jmh.annotations._
 
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+
 /**
- * Seq.applyの方がはやい！
- * [info] Benchmark        Mode  Cnt         Score        Error  Units
- * [info] SeqOrList.list  thrpt  200  11485516.282 ± 346706.969  ops/s
- * [info] SeqOrList.seq   thrpt  200  12462881.306 ± 545318.664  ops/s
+ * [info] Benchmark             Mode  Cnt          Score         Error  Units
+ * [info] SeqOrList.listApply  thrpt  200   11780452.145 ±  123376.558  ops/s
+ * [info] SeqOrList.listColon  thrpt  200  109701449.482 ± 4825541.777  ops/s
+ * [info] SeqOrList.seq        thrpt  200   13267611.620 ±  197315.652  ops/s
+ * [info] SeqOrList.vector     thrpt  200   12520556.750 ±   38746.670  ops/s
+ *
  * sbt 'project performance' 'jmh:run -i 20 -wi 20 .*SeqOrList.*'
  */
 @State(Scope.Benchmark)
 class SeqOrList {
-  val NUM = 10000
+  var NUM: Int = _
   val targets: Seq[Int] = 1 to NUM
+
+  @Setup
+  def setup() = NUM = Random.nextInt(1000)
 
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
   def seq(): Seq[Int] = {
-    Seq.apply(1)
+    Seq.apply(NUM)
   }
 
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
-  def list(): List[Int] = {
-    List.apply(1)
+  def listApply(): Seq[Int] = {
+    List.apply(NUM)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  def listColon(): Seq[Int] = {
+    NUM :: Nil
+    new ListBuffer
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  def vector(): Seq[Int] = {
+    Vector.apply(1)
   }
 }
