bff5a4f403cab5f461e2cf844ebc18ddd618b1bf
petitviolet <violethero0820@gmail.com>
2017-09-24 23:19:25 +0900

    logging with .log()

--- modules/sandbox/src/main/scala/net/petitviolet/sandbox/Logging.scala
+++ modules/sandbox/src/main/scala/net/petitviolet/sandbox/Logging.scala
@@ -7,4 +7,9 @@ trait Logging {
     val tag = s"<$clazz:${line.value }>"
     println(s"$tag $msg")
   }
+
+  implicit class Any4Logging[A](val a: A) {
+    def log()(implicit line: sourcecode.Line, enclosing: sourcecode.Enclosing): A =
+    { logging(a)(line, enclosing); a }
+  }
 }
